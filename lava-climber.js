248d7343f930cb361330d70d442330818a9e928e
Luis Pires <luisbpires@hotmail.com>
2020-06-02 20:04:51 +0100

    adding structure

--- lava-climber.js
+++ lava-climber.js
@@ -1,6 +1,16 @@
+// THREEJS RELATED VARIABLES
 let scene,camera,renderer;
+
+//LIGHTS 
+let hemisphereLight, directionalLight, directionalLightHelper;
+
+//Character
+let bob
+
 window.onload = function init(){
    createScene()
+   animate()
+   createCharacter()
 }
 
 function createScene(){
@@ -9,9 +19,9 @@ function createScene(){
      camera = new THREE.PerspectiveCamera(60, window.innerWidth/ window.innerHeight,1,10000)
 
      renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth,window.innerHeight);
+     renderer.setSize(window.innerWidth,window.innerHeight);
 
-    renderer.setClearColor("red");
+    renderer.setClearColor("#F08080");
 
         // add the output of the renderer to the DIV with id "world"
         document.getElementById('world').appendChild(renderer.domElement);
@@ -27,4 +37,34 @@ function handleWindowResize() {
     renderer.setSize(WIDTH, HEIGHT);
     camera.aspect = WIDTH / HEIGHT;
     camera.updateProjectionMatrix();
+}
+
+
+
+function createCharacter(){
+
+    bob = new THREE.Object3D();
+    // scale it down
+    //plane.scale.set(0.25, 0.25, 0.25);
+    bob.position.y = 100
+    var cyan = new THREE.Color("rgb(0, 255, 255)");
+
+    let geomBody = new THREE.BoxGeometry(60, 50, 50)
+
+    let body = new THREE.Mesh(geomBody, cyan);
+
+    bob.add(body)
+    console.log("Bob created")
+    scene.add(bob)
+}
+
+
+
+function animate() {
+ 
+    // render
+    renderer.render(scene, camera);
+
+
+    requestAnimationFrame(animate);
 }
